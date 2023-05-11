c86e6777eda0fdcdc2f82ba30942144c6b2bc8e7
Sean Montague <stmontague1@gmail.com>
2023-05-11 09:25:10 -0500

    Add material ui and update sign up form to use material ui components

--- src/sign-up/SignUp.tsx
+++ src/sign-up/SignUp.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { TextField, Button, Box, Typography, Grid } from "@mui/material";
 
 interface FormData {
     name: string;
@@ -32,42 +33,49 @@ const SignUp: React.FC<SignUpProps> = ({ onSubmit }) => {
     };
 
     return (
-        <form onSubmit={handleSubmit}>
-            <label htmlFor="name">
-                Name:
-                <input
-                    type="text"
-                    id="name"
-                    name="name"
-                    value={formData.name}
-                    onChange={handleChange}
-                />
-            </label>
-            <br />
-            <label htmlFor="email">
-                Email:
-                <input
-                    type="email"
-                    id="email"
-                    name="email"
-                    value={formData.email}
-                    onChange={handleChange}
-                />
-            </label>
-            <br />
-            <label htmlFor="password">
-                Password:
-                <input
-                    type="password"
-                    id="password"
-                    name="password"
-                    value={formData.password}
-                    onChange={handleChange}
-                />
-            </label>
-            <br />
-            <button type="submit">Submit</button>
-        </form>
+        <Grid container justifyContent="center" alignItems="center" style={{ minHeight: '100vh' }}>
+            <Grid item xs={12} sm={6} md={4}>
+                <form onSubmit={handleSubmit}>
+                    <Typography variant="h5" align="center">Sign Up</Typography>
+                    <Box margin={1}>
+                        <TextField
+                            fullWidth
+                            label="Name"
+                            name="name"
+                            value={formData.name}
+                            onChange={handleChange}
+                            variant="outlined"
+                        />
+                    </Box>
+                    <Box margin={1}>
+                        <TextField
+                            fullWidth
+                            label="Email"
+                            name="email"
+                            value={formData.email}
+                            onChange={handleChange}
+                            variant="outlined"
+                        />
+                    </Box>
+                    <Box margin={1}>
+                        <TextField
+                            fullWidth
+                            label="Password"
+                            name="password"
+                            type="password"
+                            value={formData.password}
+                            onChange={handleChange}
+                            variant="outlined"
+                        />
+                    </Box>
+                    <Box margin={1}>
+                        <Button variant="contained" color="primary" type="submit" fullWidth>
+                            Submit
+                        </Button>
+                    </Box>
+                </form>
+            </Grid>
+        </Grid>
     );
 };
 
